e1354875e7d884ec39a77c806b3a13e450f295a9
Vampirality <158781012+Vampirality@users.noreply.github.com>
2024-02-04 06:10:48 +0700

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App extends RealOperation {
+public class Main extends RealOperation {
     public static void main(String[] args) {
         Scanner scStr = new Scanner(System.in);
         Scanner scInt = new Scanner(System.in);
@@ -9,12 +9,6 @@ public class App extends RealOperation {
         int choice;
 
         while (true) {
-            System.out.print("Enter m : ");
-            m = new Real(scStr.nextLine());
-
-            System.out.print("Enter n : ");
-            n = new Real(scStr.nextLine());
-
             System.out.println("Which operation do you want to do with m and n");
             System.out.println("\t1 addition");
             System.out.println("\t2 subtraction");
@@ -28,6 +22,13 @@ public class App extends RealOperation {
             if (choice == 0) {
                 break;
             }
+
+            System.out.print("Enter m : ");
+            m = new Real(scStr.nextLine());
+
+            System.out.print("Enter n : ");
+            n = new Real(scStr.nextLine());
+
             if (choice == 1) {
                 start = System.currentTimeMillis();
                 result = RealOperation.add(m,n);
