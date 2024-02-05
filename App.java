e07db96041558db2d83f8ddad67d48f48d026895
Vampirality <158781012+Vampirality@users.noreply.github.com>
2024-02-06 00:30:10 +0700

    Update App.java

--- App.java
+++ App.java
@@ -5,23 +5,43 @@ public class Main extends RealOperation {
         Scanner scStr = new Scanner(System.in);
         Scanner scInt = new Scanner(System.in);
         Real m, n, result;
-        int choice, point;
+        int choice, choiceSame, point;
+        boolean inputBool;
 
+        choice = 0;
+        inputBool = true;
         while (true) {
-            System.out.println("Which operation do you want to do with m and n");
-            System.out.println("\t1 addition");
-            System.out.println("\t2 subtraction");
-            System.out.println("\t3 multiplication");
-            System.out.println("\t4 division (30 places decimal output)");
-            System.out.println("\t5 division (customizable places decimal output)");
-            System.out.println("\t6 division (integer output)");
-            System.out.println("\t7 modulo");
-            System.out.println("0 stop the program.");
-            System.out.print(": ");
-            choice = scInt.nextInt();
+            if (choice > 0 && choice <= 8) {
+                choiceSame = 2;
+                while (choiceSame != 0 && choiceSame != 1) {
+                    System.out.println("Do you want to do same operation???");
+                    System.out.println("\t1 yes");
+                    System.out.println("\t0 no");
+                    System.out.print(": ");
+                    choiceSame = scInt.nextInt();
+                }
+                inputBool = choiceSame == 0;
+            }
+
+            if (inputBool) {
+                System.out.println("Which operation do you want to do with m and n???");
+                System.out.println("\t1 addition");
+                System.out.println("\t2 subtraction");
+                System.out.println("\t3 multiplication");
+                System.out.println("\t4 division (30 places decimal output)");
+                System.out.println("\t5 division (customizable places decimal output)");
+                System.out.println("\t6 division (integer output)");
+                System.out.println("\t7 modulo");
+                System.out.println("0 stop the program.");
+                System.out.print(": ");
+                choice = scInt.nextInt();
+            }
 
             if (choice == 0) {
                 break;
+            }
+            if (choice < 0 || choice > 7) {
+                continue;
             }
 
             System.out.println("--------------------------------------------");
@@ -58,16 +78,17 @@ public class Main extends RealOperation {
                 result = RealOperation.divideInteger(m,n);
                 System.out.println(m.getFullNumber() + " % " + n.getFullNumber());
             }
-            else if (choice == 7) {
+            else {
                 result = RealOperation.modulo(m,n);
                 System.out.println(m.getFullNumber() + " % " + n.getFullNumber());
-            }
-            else {
-                continue;
             }
 
             System.out.println("= " + result.getFullNumber());
             System.out.println("--------------------------------------------");
         }
+        System.out.println(" ---------------------------------------");
+        System.out.println("|     E N D   T H E   P R O G R A M     |");
+        System.out.println("|               made by \" Vampirality \" |");
+        System.out.println(" ---------------------------------------");
     }
 }
