363f2912d8351e009f1b05376e80698fbfe6dc9e
Vampirality <158781012+Vampirality@users.noreply.github.com>
2024-02-05 20:15:00 +0700

    Update App.java

--- App.java
+++ App.java
@@ -5,17 +5,17 @@ public class Main extends RealOperation {
         Scanner scStr = new Scanner(System.in);
         Scanner scInt = new Scanner(System.in);
         Real m, n, result;
-        long start, end;
-        int choice;
+        int choice, point;
 
         while (true) {
             System.out.println("Which operation do you want to do with m and n");
             System.out.println("\t1 addition");
             System.out.println("\t2 subtraction");
             System.out.println("\t3 multiplication");
-            System.out.println("\t4 integer division");
-            System.out.println("\t5 division");
-            System.out.println("\t6 modulo");
+            System.out.println("\t4 division (30 places decimal output)");
+            System.out.println("\t5 division (customizable places decimal output)");
+            System.out.println("\t6 division (integer output)");
+            System.out.println("\t7 modulo");
             System.out.println("0 stop the program.");
             System.out.print(": ");
             choice = scInt.nextInt();
@@ -33,39 +33,33 @@ public class Main extends RealOperation {
             n = new Real(scStr.nextLine());
 
             if (choice == 1) {
-                start = System.currentTimeMillis();
                 result = RealOperation.add(m,n);
-                end = System.currentTimeMillis();
                 System.out.println(m.getFullNumber() + " + " + n.getFullNumber());
             }
             else if (choice == 2) {
-                start = System.currentTimeMillis();
                 result = RealOperation.minus(m,n);
-                end = System.currentTimeMillis();
                 System.out.println(m.getFullNumber() + " - " + n.getFullNumber());
             }
             else if (choice == 3) {
-                start = System.currentTimeMillis();
                 result = RealOperation.multiply(m,n);
-                end = System.currentTimeMillis();
                 System.out.println(m.getFullNumber() + " * " + n.getFullNumber());
             }
             else if (choice == 4) {
-                start = System.currentTimeMillis();
-                result = RealOperation.divideInteger(m,n);
-                end = System.currentTimeMillis();
+                result = RealOperation.divide(m,n);
                 System.out.println(m.getFullNumber() + " / " + n.getFullNumber());
             }
             else if (choice == 5) {
-                start = System.currentTimeMillis();
-                result = RealOperation.divide(m,n);
-                end = System.currentTimeMillis();
+                System.out.print("\tEnter a decimal point : ");
+                point = scInt.nextInt();
+                result = RealOperation.divide(m,n,point);
                 System.out.println(m.getFullNumber() + " / " + n.getFullNumber());
             }
             else if (choice == 6) {
-                start = System.currentTimeMillis();
+                result = RealOperation.divideInteger(m,n);
+                System.out.println(m.getFullNumber() + " % " + n.getFullNumber());
+            }
+            else if (choice == 7) {
                 result = RealOperation.modulo(m,n);
-                end = System.currentTimeMillis();
                 System.out.println(m.getFullNumber() + " % " + n.getFullNumber());
             }
             else {
@@ -73,7 +67,6 @@ public class Main extends RealOperation {
             }
 
             System.out.println("= " + result.getFullNumber());
-            System.out.println("Estimate time : "+(end - start)+" ms");
             System.out.println("--------------------------------------------");
         }
     }
